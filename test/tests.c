67ac326cb855e1743652fa3ccd9b3b47220ff4e7
Pierre Genthon <genthon.pierre@gmail.com>
2017-02-12 18:48:16 +0100

    Adds tests

--- test/tests.c
+++ test/tests.c
@@ -16,6 +16,25 @@ void test_Board_initRandom() {
     CU_ASSERT_FALSE(areSimilarBoards(b1, b2, BSIZE));
 }
 
+void test_Board_floodBoard() {
+    board b1 = (Cell[9]){
+        R, G, B,
+        R, B, Y,
+        M, B, Y
+    };
+    Cell oldColor = getBoardCell(b1, 3, x, y);
+    Cell newColor = B;
+    board b2 = (Cell[9]){
+        B, G, B,
+        B, B, Y,
+        M, B, Y
+    };
+    floodBoard(b, 3, oldColor, newColor, 0, 0);
+    CU_ASSERT(areSimilarBoards(b, expectedAfter, 3));
+}
+
+
+
 int main() {
     CU_pSuite pSuite = NULL;
     CU_initialize_registry();
@@ -24,6 +43,7 @@ int main() {
 
     CU_add_test(pSuite, "initBoard() should return a non null pointer to a Cell", test_Board_initBoard);
     CU_add_test(pSuite, "initRandomBoard() should produce a different board each time it is executed", test_Board_initRandom);
+    CU_add_test(pSuite, "floodBoard() should flood correctly", test_Board_floodBoard);
 
     // output to stdout
     CU_basic_set_mode(CU_BRM_VERBOSE);
