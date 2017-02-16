652bdb14478f459808c429a5dd351566b0d0aaca
Ellymac <cschnell67@gmail.com>
2017-02-16 18:20:59 +0100

    updated main.c : changed function's names and added an updateBoard function

--- src/main.c
+++ src/main.c
@@ -7,44 +7,7 @@
 #include <SDL2/SDL.h>
 #include "board.h"
 
-/**
- * \fn
- * \return int
- **/
-int main() {
-    void* nullptr = NULL;
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        printf("SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, MAX_SIZE * 20, MAX_SIZE * 20, SDL_WINDOW_SHOWN);
-    if (win == nullptr) {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (ren == nullptr) {
-        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    // Set background to white
-    SDL_SetRenderDrawColor( ren, 0, 0, 0, 255 );
-
-    // Clear window
-    SDL_RenderClear( ren );
-
-    Board* board = initRandomBoard(MAX_SIZE);
-
-    display_board(board, ren);
-    check_events(board, ren, win);
-    return 0;
-}
-
-void display_board(Board* board, SDL_Renderer* ren) {
+void displayBoard(Board* board, SDL_Renderer* ren) {
   for(size_t x = 0; x < 24; x++) {
     for(size_t y = 0; y < 24; y++) {
       // Init rect
@@ -84,24 +47,63 @@ void display_board(Board* board, SDL_Renderer* ren) {
   SDL_RenderPresent(ren);
 }
 
-void check_events(Board* board, SDL_Renderer* ren, SDL_Window* win) {
-  SDL_Event mouse_event;
-  char color;
+void updateBoard(Board* board, size_t x, size_t y, SDL_Renderer* ren) {
+  char color = getBoardCell(board, x, y);
+  floodBoard(board, getBoardCell(board, 0, 0), color, 0, 0);
+  displayBoard(board, ren);
+}
 
-  while(1) {
-    SDL_WaitEvent(&mouse_event);
-    switch(mouse_event.type) {
-      case SDL_QUIT:
-        SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
-        SDL_Quit();
-        return 0;
-        break;
-      case SDL_MOUSEBUTTONDOWN:
-        color = getBoardCell(board, mouse_event.button.x / 21, mouse_event.button.y / 21);
-        floodBoard(board, getBoardCell(board, 0, 0), color, 0, 0);
-        display_board(board, ren);
-        break;
-    }
+void checkEvents(Board* board, SDL_Event mouse_event, SDL_Renderer* ren, SDL_Window* win) {
+  switch(mouse_event.type) {
+    case SDL_QUIT:
+      SDL_DestroyRenderer(ren);
+      SDL_DestroyWindow(win);
+      SDL_Quit();
+      exit(0);
+      break;
+    case SDL_MOUSEBUTTONDOWN:
+      updateBoard(board, mouse_event.button.x / 21, mouse_event.button.y / 21, ren);
+      break;
   }
+}
+
+/**
+ * \fn
+ * \return int
+ **/
+int main() {
+    void* nullptr = NULL;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *win = SDL_CreateWindow("Color Flood", 100, 100, MAX_SIZE * 20, MAX_SIZE * 20, SDL_WINDOW_SHOWN);
+    if (win == nullptr) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr) {
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Set background to white
+    SDL_SetRenderDrawColor( ren, 0, 0, 0, 255 );
+
+    // Clear window
+    SDL_RenderClear( ren );
+
+    Board* board = initRandomBoard(MAX_SIZE);
+    SDL_Event mouse_event;
+
+    displayBoard(board, ren);
+    while(1) {
+      SDL_WaitEvent(&mouse_event);
+      checkEvents(board, mouse_event, ren, win);
+    }
+    return 0;
 }
