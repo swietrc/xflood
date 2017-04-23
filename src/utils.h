5fa674a8f0fc8047416510df8d9fb6c74b8dd20e
Swietrich <simon.wietrich@gmail.com>
2017-04-23 18:13:45 +0200

    Improve button drawing function
    + drawButton() now separates the text position and the button position thus allowing for the addition of margins.
    
    + Window size values are now stored in utils.h

--- src/utils.h
+++ src/utils.h
@@ -5,7 +5,13 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
+#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 620
+
+#define FONT_DIR "./vendor/fonts"
+
 TTF_Font* defaultFont;
+TTF_Font* buttonFont;
 
 void drawButton(char* txt, int x, int y, int w, int h, SDL_Color txtColor, SDL_Color backgroundColor, SDL_Renderer* ren);
 
