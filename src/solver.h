adceedaa61fe9a94c0ec7be533ec16d2d14f7131
Simon Wietrich <simon.wietrich@gmail.com>
2017-05-10 16:06:32 +0200

    Add heuristic algorithm to solve the board in less than 10k years

--- src/solver.h
+++ src/solver.h
@@ -5,6 +5,8 @@
 #include "board.h"
 
 ColorList* getPossibleColors(Board* b);
+void getColoredAreaSize(Board* b, bool* visitedTiles, size_t x, size_t y, size_t* size);
 int solveBoard(Board* b, ColorList* bestSolution, ColorList* currentSolution);
+int solveBoardEfficient(Board* b, ColorList* solution);
 
 #endif
